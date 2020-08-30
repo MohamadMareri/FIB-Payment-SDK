59e43aa61e798219c91fd750408e14fcef714524
mohamad.mareri <mohamad.mareri.eng@gmail.com>
2020-08-30 23:01:14 +0200

    Add Sample Project

---
+++ Carthage/Build/iOS/FIB_Payment_SDK.framework/Headers/FIB_Payment_SDK.h
@@ -0,0 +1,19 @@
+//
+//  FIB_Payment_SDK.h
+//  FIB-Payment-SDK
+//
+//  Created by Mohamad Mareri on 30.08.20.
+//  Copyright © 2020 Mohamad Mareri. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FIB_Payment_SDK.
+FOUNDATION_EXPORT double FIB_Payment_SDKVersionNumber;
+
+//! Project version string for FIB_Payment_SDK.
+FOUNDATION_EXPORT const unsigned char FIB_Payment_SDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FIB_Payment_SDK/PublicHeader.h>
+
+
---
+++ FIB-Payment-SDK/Sample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Sample
+//
+//  Created by Mohamad Mareri on 30.08.20.
+//  Copyright © 2020 Mohamad Mareri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
